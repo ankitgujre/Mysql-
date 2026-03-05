494e556c0f5a5b6a1e4f6040afff37bf09ec1fd5
ankitgujre <136592231+ankitgujre@users.noreply.github.com>
2026-03-05 23:19:07 +0530

    creating three tables

---
+++ HomeWork.sql
@@ -0,0 +1,21 @@
+create database my_db1;
+use my_db1;
+
+create table my_tab1(
+	roll_no int, student_name varchar(20),
+    address varchar(30)
+    );
+    
+create table my_tab2(
+	emp_id int, emp_name varchar(20),
+    city varchar(30), joining_date date
+    );
+    
+    
+create table my_tab3(
+	order_id int, product_name varchar(20),
+    address varchar(30), email_id varchar(10)
+    );
+
+
+-- desc my_tab3;
